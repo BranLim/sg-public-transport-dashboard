16c113a621fe06e258ce346ab61bf73d08dae2ab
Brandon Lim <limws.brandon@gmail.com>
2021-01-25 22:07:31 +0800

    Add current date/time display

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LtaApiService } from './LtaApiService';
 import { BusStop } from './shared/models/bus-stop.model';
+import { formatDate } from '@angular/common';
 
 @Component({
     selector: 'transport-dashboard',
@@ -12,18 +13,27 @@ export class DashboardComponent implements OnInit {
     private ltaApiService: LtaApiService;
     transports: BusStop[] = [
         { busStopCode: '1234', busNumber: '99', busStopName: 'Blk 123' },
-    { busStopCode: '1234', busNumber: '242', busStopName: 'Blk 123' },
-    { busStopCode: '7584', busNumber: '193', busStopName: 'Blk 222' },
-    { busStopCode: '7584', busNumber: '242', busStopName: 'Blk 222' },
-    { busStopCode: '4324', busNumber: '185', busStopName: 'Blk 234' },
-    { busStopCode: '4324', busNumber: '99', busStopName: 'Blk 234' }];
+        { busStopCode: '1234', busNumber: '242', busStopName: 'Blk 123' },
+        { busStopCode: '7584', busNumber: '193', busStopName: 'Blk 222' },
+        { busStopCode: '7584', busNumber: '242', busStopName: 'Blk 222' },
+        { busStopCode: '4324', busNumber: '185', busStopName: 'Blk 234' },
+        { busStopCode: '4324', busNumber: '99', busStopName: 'Blk 234' }];
+
+    currentTime?: string;
+
 
     constructor(ltaApiService: LtaApiService) {
         this.ltaApiService = ltaApiService;
     }
 
     ngOnInit() {
+        this.startTimer();
+    }
 
+    private startTimer() {
+        setInterval(() => {
+            this.currentTime = formatDate(new Date(), 'dd-MM-yyyy HH:mm:ss', 'en', '+8');
+        }, 1000);
     }
 
 }
